afbde6541de8e78949bae26f083c0550fca6bbe7
GemaNVG <gnvazquez@itparral.edu.mx>
2025-12-15 23:52:31 -0600

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,15 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('table');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/report', function () {
-    return view('report');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-// ->middleware(['auth'])->name('dashboard');
+require __DIR__.'/auth.php';
